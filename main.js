ce6ff654554a93f6c99a0397c43b1100536975cc
madiha <madihakanwal655@gmail.com>
2025-10-07 09:25:08 +0500

    js

---
+++ main.js
@@ -0,0 +1,254 @@
+// let a=0;
+// var b=5;
+// const c=10;
+
+// console .log("hello from js");
+// console.log("a");
+// document.getElementById("para").innerHTML=c;
+// let a=5;
+// let b=7;
+// let c=8;
+
+
+
+// let d=a*b;
+// let e=a/b;
+// let f=b;
+// let g=b+c;
+// let k=a-b;
+
+
+// console.log(d);
+// console.log (e);
+// console.log(f);
+// console.log(g);
+// console.log(k);
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log(2+2);
+// const a=2;
+// const b=5;
+
+//  if(a<b)
+//  console.log(" a is smaller than b")
+
+//  console.log(" i am out of if statement")
+
+//  if(a>b){
+
+//  console.log("a is smaller than b")
+
+//  }
+// if(a<2)
+//     console.log("a is less than 2")
+
+// if(2<=2){
+//     console.log("a is less than or equal 2");
+//   }  
+
+// else{
+//     console.log("a is greater than 2");
+    
+// }
+// const a=5
+// const b=100
+// if(a>100){
+//     console.log("a is greater than 100")
+    
+// }
+// let username="admin"
+
+// if(username=="admin"){
+//     console.log("welcome")
+// }
+// let marks=100
+
+// if (marks>=50) {
+//     console.log("pass")
+    
+// }
+// let age=18
+// if(age>=18){
+//     console.log("adult")
+// }
+// else{
+// console.log("minor")
+    
+// }
+// let temperature=30;
+// if(temperature>30){
+//     console.log("hot")
+    
+// }
+// else{
+//     console.log("cool")
+    
+// }
+// let password=1234;
+
+// if(password==1234){
+//     console.log("access granted") 
+// }
+// else{
+//     console.log("access denied")
+    
+// }
+// let day="sunday";
+// if (day=="sunday") {
+//     console.log("holiday");
+    
+// }
+// else{
+//     console.log("working day");
+    
+// }
+
+// let num1=3;
+// let num2=2;
+
+//   if (num1>num2) {
+//     console.log("first is larger");
+    
+//   }
+//   else{
+//     console.log("second is larger or equal");
+    
+//   }
+//   let loggedin="true";
+//   if (loggedin=="true") {
+//     console.log("dashboard");
+    
+//   } else {
+//     console.log("login page");
+    
+//   }
+// if else if
+    // let a= -100
+    // if (a<5) {
+    //     console.log("a is less than 5");
+        
+    // } else if (a>5) {
+    //     console.log("a is greater than 5");
+        
+    // } else {
+        
+    // }{
+    //     console.log("a is equal to 5");
+        
+    // };
+    // terray
+    // let age=23;
+    //  let text = (age<23) ? "minor" : "adult";
+    //  (age<18) ? console.log("minor") : console.log("adult");
+    //  console.log(text);
+    //  for loop
+     
+//     for (let i=0; i<5; i++)
+//          {
+//     console.log("pakistan");
+       
+//     }
+     
+// while loop
+
+// let i=0;
+// while (i<10) {
+//      console.log(i);
+//      i++
+// }
+// string
+
+
+// let intro="my name is 'madiha'";
+// let intro1='my name is "madiha"';
+// console.log(intro);
+// console.log(intro1);
+
+// tamplate string
+
+// let car=`car is a"special" vehicles`;
+// console.log(car);
+
+// const seats=4;
+
+// let car1=`car is a ${seats} vehicles`;
+// console.log(car1);
+
+// string methods
+
+// let course="This is the Web Application Development";
+// console.log(course.length);
+
+// console.log(course.to);
+// console.log(course.toLowerCase(course));
+// console.log(course.toUpperCase (course));
+
+// let id=3312;
+// console.log(id.toString());
+
+// let universe=`
+// our universe is made galaxies and each galaxy have its own stars`;
+// console.log(universe.length);
+// console.log(universe.charAt(6));
+// console.log(universe.charCodeAt(1));
+// console.log(universe.codePointAt(1));
+
+// let emoji="😊"
+// console.log(emoji.charCodeAt(0));
+// console.log(universe.at(2));
+
+
+// const first_name= "madiha";
+// const last_name= "kanwal";
+// const full_name=first_name.concat(` `,last_name);
+// const name=first_name.concat(last_name);
+// console.log(name);
+// console.log(full_name);
+
+// let text= "hello world! \uD800"
+// console.log(text.isWellFormed());
+
+// let text1= "hello world! \uD800"
+// console.log(text1.toWellFormed());
+
+// const name1 = "W3Schools";
+// let letter = name[2];
+// console.log(letter);
+
+
+
+// let text2 = "Apple, Banana, Kiwi";
+// let part = text2.slice(7, 13);
+// console.log(part);
+
+// let text3 = "Apple, Banana, Kiwi";
+// let part1 = text.slice(-12, -6);
+// console.log(part1);
+
+// let str = "Apple, Banana, Kiwi";
+// let part2 = str.substr(7, 6);
+// console.log(part2);
+
+
+// let text4 = "      Hello World!      ";
+// let text5 = text1.trim();
+// console.log(text4);
+// console.log(text5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
